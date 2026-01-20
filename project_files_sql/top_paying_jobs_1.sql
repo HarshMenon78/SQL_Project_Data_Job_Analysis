2e8dff25a532208590ce107e9505754d7df697d5
Harsh Menon <harshmenon78@gmail.com>
2026-01-20 18:27:42 +0530

    query 1 made and editted

---
+++ project_files_sql/top_paying_jobs_1.sql
@@ -0,0 +1,23 @@
+/*
+Question: What are the top paying data analyst jobs posted:
+- Identify the top 10 highest paying data analyst job postings , which are available remotely.
+- Focusus on jobs with specific salaries , which are not NULL.
+- Also mention the company name and location for each job posting.
+*/
+SELECT
+    job_id,
+    job_title_short,
+    jpf.company_id,
+    c.name AS company_name,
+    job_location,
+    salary_year_avg
+FROM
+    job_postings_fact AS jpf
+LEFT JOIN 
+    company_dim AS c ON jpf.company_id = c.company_id
+WHERE
+    job_location = 'Anywhere'-- job_location set to 'Anywhere' specifies remote-job/wfh
+    AND salary_year_avg IS NOT NULL
+ORDER BY
+    salary_year_avg DESC
+LIMIT 10;
